0e1bea13293117c33781a3bdf6c03ce5a2ff5067
H.N. Gibson <revhngibson@gmail.com>
2025-05-12 11:01:51 -0500

    WIP Assignment

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// Email Verification
+
+// Save Sign-up Data
+
+// Post Sign-up Data to Public List of Organizations
+
+// Update Sign-up Data
+
+// Verify Sign-up Data
+
+// Verify Feedback Submission
