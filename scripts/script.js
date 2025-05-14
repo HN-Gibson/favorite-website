89b2fa1acc213cc5292f3726284cdecad56726b6
H.N. Gibson <revhngibson@gmail.com>
2025-05-14 09:43:22 -0500

    WIP Assignment

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,58 @@
+// Feedback Storage
+
+let feedback = [];
+
+function addFeedback(message) {
+  feedback.push(message);
+  return feedback;
+}
+
+// Verify Feedback Submission
+
+function validateFeedback(submittedFeedback){
+    if (submittedFeedback !== "") {
+        submittedFeedback.addFeedback();
+    } else {
+        return ("No Feedback Entered.");
+    }
+}
+
 // Email Verification
 
+function validateEmail(email){
+    if (email.includes("@") && email.includes(".")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Phone Number Verification
+
+function validatePhone(phone){
+    const phoneRegex = /^\+?([0-9]{1,3})?[-. ]?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (phoneRegex.test(phone) === true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 // Save Sign-up Data
+
+let userData = []
+
+function saveUserData (submittedUserData){
+    let validEmail = submittedUserData[1].validateEmail();
+    let validPhone = submittedUserData[2].validatePhone();
+    if (validEmail === true && validPhone === true) {
+        userData.push(submittedUserData);
+        return userData;
+    } else {
+        return ("Email or Phone could not be verified.")
+    }
+}
 
 // Post Sign-up Data to Public List of Organizations
 
 // Update Sign-up Data
-
-// Verify Sign-up Data
-
-// Verify Feedback Submission
